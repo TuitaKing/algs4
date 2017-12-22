1538ba843a94a2c9b5c0f1023445e9cc12e520a9
TuitaKing <735367737@qq.com>
2017-12-22 21:37:16 +0800

    归并排序的递归版

--- src/com/lj/algs4/sort/MergeSort.java
+++ src/com/lj/algs4/sort/MergeSort.java
@@ -1,34 +1,62 @@
 package com.lj.algs4.sort;
+
 //采用遍历的方式
 public class MergeSort extends AbstractSort {
+
+    private static Comparable[] result;
+
     @Override
     public void sort(Comparable[] a) {
-        Comparable[] temp=new Comparable[a.length];
-        for(int block=1; block<2*a.length;block*=2){//最外层循环表示归并的子序列中的元素个数
-            for(int start=0 ; start<a.length ; start+=2*block){ //按照上面提到的元素个数对每一组进行排序
-                int indext=start;
-                int mid=(start+block)>a.length ? a.length:(start+block);
-                int end=(start+2*block)>a.length?a.length:(start+2*block);
-                int loStart=start,loEnd=mid;
-                int hiStart=mid,hiEnd=end;
-                while(loStart<loEnd && hiStart < hiEnd){
-                    temp[indext++]=a[loStart].compareTo(a[hiStart])>0?a[hiStart++]:a[loStart++];
+        Comparable[] temp = new Comparable[a.length];
+        for (int block = 1; block < 2 * a.length; block *= 2) {//最外层循环表示归并的子序列中的元素个数
+            for (int start = 0; start < a.length; start += 2 * block) { //按照上面提到的元素个数对每一组进行排序
+                int indext = start;
+                int mid = (start + block) > a.length ? a.length : (start + block);
+                int end = (start + 2 * block) > a.length ? a.length : (start + 2 * block);
+                int loStart = start, loEnd = mid;
+                int hiStart = mid, hiEnd = end;
+                while (loStart < loEnd && hiStart < hiEnd) {
+                    temp[indext++] = a[loStart].compareTo(a[hiStart]) > 0 ? a[hiStart++] : a[loStart++];
                 }
-                while(loStart<loEnd){
-                    temp[indext++]=a[loStart++];
+                while (loStart < loEnd) {
+                    temp[indext++] = a[loStart++];
                 }
-                while(hiStart<hiEnd){
-                    temp[indext++]=a[hiStart++];
+                while (hiStart < hiEnd) {
+                    temp[indext++] = a[hiStart++];
                 }
             }
-             Comparable[] cap=a;
-             a=temp;
-             temp=cap;
+            Comparable[] cap = a;
+            a = temp;
+            temp = cap;
         }
         temp = a;
         show(a);
     }
-    private void sort(Comparable[] a ,int lo , int hi){
+    public static void megre_sort(Comparable[] a){
+        result=new Comparable[a.length];
+        sort(a,0,a.length-1);
+    }
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (lo >= hi)
+            return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        int start1 = lo, end1 = mid;
+        int start2 = mid + 1, end2 = hi;
+        int index = lo;
+        for (int k = index; k <= hi; k++) {
+            result[k] = a[k];
+        }
+        while (start1 <= end1 && start2 <= end2) {
+            a[index++] = result[start1].compareTo(result[start2]) < 0 ? result[start1++] : result[start2++];
+        }
+        while (start1 <= end1) {
+            a[index++] = result[start1++];
+        }
+        while (start2 <= end2) {
+            a[index++] = result[start2++];
+        }
 
 
     }
